0e95681e584b4a28129ea0dc332b2b810aaac01f
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-06-12 15:16:11 +0500

    changes-8

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,22 +5,33 @@ import User from "../models/User.js";
 
 export async function authenticateUser(req, res, next) {
   try {
-    // console.log("authorization=>", req.headers.authorization);
-    const bearerToken = req?.headers?.authorization;
-    const token = bearerToken?.split(" ")[1];
-    if (!token) return sendResponse(res, 403, null, true, "Token Not Provide");
-    // res.send("Working on Update API");
+
+    // const bearerToken = req?.headers?.authorization;
+    // const token = bearerToken?.split(" ")[1];    
+
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) return sendResponse(res, 403, null, true, "Token Not Provided");
+
     const decoded = jwt.verify(token, process.env.AUTH_SECRET);
+
     if(decoded){
+
         const user = await User.findById(decoded._id);
+
         if(!user) return sendResponse(res, 403, null, true, "User Not Found")
+
         req.user = decoded;
+
         next();
+
     }else{
-            sendResponse(res, 500, null, true, "Something went Worong");
+
+            sendResponse(res, 500, null, true, "Something Went Worong");
     }
   } catch (err) {
-    sendResponse(res, 500, null, true, "Something went Worong");
+
+    sendResponse(res, 500, null, true, "Something Went Worong");
   }
 }
 
